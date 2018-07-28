8b57f99d75d8029568d2b369df96796d62e807a6
Alan Ficagna <alan.ficagna@gmail.com>
2018-07-28 18:30:06 -0300

    Add first screen

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,76 @@
 module Main exposing (..)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
 
 -- component import example
-import Components.Hello exposing ( hello )
+
+import Components.Hello exposing (hello)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 -- APP
+
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
+
+
+type alias Model =
+    { email : String
+    }
+
 
 model : Model
-model = 0
+model =
+    { email = ""
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | Increment
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        Increment ->
+            model
+
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    main_ []
+        [ section [ class "jumbotron full-screen flex-column-evenly-center" ]
+            (welcome model)
         ]
-      ]
+
+
+welcome : { email : email } -> List (Html Msg)
+welcome email =
+    [ h2 [] [ text "Before we begin, please tell me your email" ]
+    , h3 [] [ text "It will only be used as an identifier. I am not gonna spam you =)" ]
+    , div [ class "container l3" ]
+        [ div [ class "form-group" ]
+            [ label [] [ text "Email" ]
+            , input [ type_ "email", class "form-control" ] []
+            ]
+        , button [ type_ "submit", class "btn btn-primary pull-right" ] [ text "Begin" ]
+        ]
     ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
