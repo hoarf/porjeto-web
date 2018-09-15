81d0f8a0ebed998f364ae54fddd39ba16c556e48
Alan Ficagna <alan.ficagna@gmail.com>
2018-09-15 16:54:48 -0300

    First attempt at encoding answers.

--- src/elm/Answer.elm
+++ src/elm/Answer.elm
@@ -1,6 +1,7 @@
 module Answer exposing (Answer, decoder, update)
 
 import Json.Decode as Decode
+import Json.Encode as Encode
 
 
 update : Answer -> Bool -> Answer
@@ -19,3 +20,9 @@ decoder =
     Decode.map2 Answer
         Decode.string
         (Decode.succeed False)
+
+
+encode : Answer -> Encode.Value
+encode answer =
+    Encode.object
+        [ ( "answer1", Encode.bool answer.value ) ]
