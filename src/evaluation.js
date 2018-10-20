215fd7876fd59e7bba333d6ba698fbc66864ccf4
Alan Ficagna <alan.ficagna@gmail.com>
2018-10-20 13:47:47 -0300

    Add react.

--- src/evaluation.js
+++ src/evaluation.js
@@ -1,6 +1,1 @@
-// pull in desired CSS/SASS files
-require( './sass/main.scss' );
-
-// inject bundled Elm app into div#main
-var Elm = require( './elm/Main' );
-Elm.Main.embed(document.querySelector('body'), null);
+import App from "./react/App.js";
